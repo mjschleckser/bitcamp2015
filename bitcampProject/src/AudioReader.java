442c53d4b97200422a115aa44ae13c529b85db7b
mjschleckser <mjschleckser@gmail.com>
2015-04-11 00:25:26 -0400

    Cont.

--- bitcampProject/src/AudioReader.java
+++ bitcampProject/src/AudioReader.java
@@ -1,10 +1,20 @@
 import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class AudioReader{
 	
-	void readFile(File f){
-		
-	}
+	private AudioInputStream audioInputStream;
+    private AudioFormat format;
+	
+    public  AudioReader(File file) throws UnsupportedAudioFileException, IOException{
+        audioInputStream = AudioSystem.getAudioInputStream(file);
+        format = audioInputStream.getFormat();
+    }
 	
 		
 }
