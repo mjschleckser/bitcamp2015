260ecb9e4c10ded5bb63e2275dfa9c805e8b3300
mjschleckser <mjschleckser@gmail.com>
2015-04-11 00:35:26 -0400

    Adding some more methods

--- bitcampProject/src/AudioReader.java
+++ bitcampProject/src/AudioReader.java
@@ -6,7 +6,7 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-// some of this is blatantly stolen from:
+// some of this is taken from:
 // http://web.archive.org/web/20120531113946/http://www.builogic.com/java/javasound-read-write.html
 
 public class AudioReader{
@@ -14,9 +14,25 @@ public class AudioReader{
 	private AudioInputStream audioInputStream;
     private AudioFormat format;
 	
-    public  AudioReader(File file) throws UnsupportedAudioFileException, IOException{
+    public AudioReader(File file) throws UnsupportedAudioFileException, IOException{
         audioInputStream = AudioSystem.getAudioInputStream(file);
         format = audioInputStream.getFormat();
+    }
+    
+    public AudioFormat getFormat() {
+        return format;
+    }
+    
+    public void read() throws IOException{
+    	int max_bytes = audioInputStream.available();
+    	
+    	
+    	return;
+    }
+    
+    public void close() throws IOException{
+    	audioInputStream.close();
+    	return;
     }
 	
 		
