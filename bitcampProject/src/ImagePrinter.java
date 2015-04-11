63d24ed582abab8ae336bbd0f1fe220292de32a7
mjschleckser <mjschleckser@gmail.com>
2015-04-11 00:42:38 -0400

    new class

---
+++ bitcampProject/src/ImagePrinter.java
@@ -0,0 +1,16 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ImagePrinter{
+	
+	public ImagePrinter(){
+		
+	}
+	
+	public void write(){
+		BufferedImage bi;
+		File f = new File("Image.png");
+	}
+	
+	
+}
